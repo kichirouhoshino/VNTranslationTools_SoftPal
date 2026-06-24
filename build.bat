49152c95250a1b35772ce786ef5c2559c108b05c
kichirouhoshino <144148399+kichirouhoshino@users.noreply.github.com>
2026-06-24 21:34:38 +0800

    Optimize build.bat to only build winmm.dll

--- build.bat
+++ build.bat
@@ -9,22 +9,11 @@ if exist Debug rmdir /S /Q Debug
 if exist Release rmdir /S /Q Release
 
 mkdir Build
-mkdir Build\VNTextPatch
 mkdir Build\VNTextProxy
 
-dotnet restore VNTextPatch\VNTextPatch.csproj /p:RuntimeIdentifiers=win
-msbuild VNTextPatch\VNTextPatch.csproj /p:LangVersion=9 /p:AllowUnsafeBlocks=true /p:Platform=AnyCPU /p:Configuration=Release /p:OutputPath=..\Build\VNTextPatch\
-del Build\VNTextPatch\FreeMote*.xml
-del Build\VNTextPatch\*.pdb
-del Build\VNTextPatch\*.txt
-
-msbuild VNTextProxy\VNTextProxy.vcxproj /p:Platform=Win32 /p:Configuration=Release /p:TargetName=d2d1
-copy /Y VNTextProxy\Release\d2d1.dll Build\VNTextProxy
+echo Building winmm...
+copy /Y VNTextProxy\AlternateProxies\winmm\*.* VNTextProxy
+msbuild VNTextProxy\VNTextProxy.vcxproj /p:Platform=Win32 /p:Configuration=Release /p:TargetName=winmm
+copy /Y VNTextProxy\Release\winmm.dll Build\VNTextProxy
 rmdir /S /Q VNTextProxy\Release
 
-for /D %%p in (VNTextProxy\AlternateProxies\*) do (
-    copy /Y VNTextProxy\AlternateProxies\%%~np\*.* VNTextProxy
-    msbuild VNTextProxy\VNTextProxy.vcxproj /p:Platform=Win32 /p:Configuration=Release /p:TargetName=%%~np
-    copy /Y VNTextProxy\Release\%%~np.dll Build\VNTextProxy
-    rmdir /S /Q VNTextProxy\Release
-)
